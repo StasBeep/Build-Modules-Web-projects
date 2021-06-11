2754483fcbd02bf7c604d43f016668068b186f13
StasBeep <stas-stas.stanislav@mail.ru>
2021-06-12 02:27:34 +0300

    Решение с зависимостью массива

--- one-point/index.js
+++ one-point/index.js
@@ -18,15 +18,29 @@ function loadScript(src, callback) {
 
 function loadScriptArr(src, callback) {
     src.forEach((el) => {
+        // Выделяем все теги script в head
+        const scriptArr = document.head.getElementsByTagName('script');
+
+        let check = true;
+
+        for (let i = 0; i < scriptArr.length; i++) {
+            if (scriptArr[i].attributes[0].nodeValue == el) {
+                check = false;
+                break;
+            }
+        }
+
+        if(check) {
         const script = document.createElement('script');
         script.src = el;
         // Три раза обработался callback
         script.onload = callback;
         document.head.insertAdjacentElement('beforeend', script);
+        }
     });
 }
 
-function loadScriptCallBack(callback){
+function loadScriptCallBack(callback) {
     const src = ['./common.js', './timer.js', './a.js']
     src.forEach((el) => {
         const script = document.createElement('script');
@@ -40,19 +54,19 @@ function loadScriptCallBack(callback){
 // Домашнее задание №1 рассмотреть три варинта реализации loadScript, при условии,
 // что в src хранятся несколько ссылок, либо вообще массив
 
-/*
+
 // Вариант с массивом
-loadScriptArr(['./common.js', './timer.js', './a.js'], () => {
+loadScriptArr(['./a.js', './common.js', './timer.js', './common.js', './b.js'], () => {
     console.log('loadScriptArr');
-});*/
+});
 
-
+/*
 // Вариант с callback
 loadScriptCallBack(() => {
     console.log('loadCallBack');
 });
 
-/*
+
 // Вариант со строкой
 loadScript('./common.js', () => {
     // Функция, которая находится в common.js
