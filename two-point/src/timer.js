2fdf5bc7a9ff2ee43ca2343f775d3ef11c6fb95f
StasBeep <stas-stas.stanislav@mail.ru>
2021-06-12 22:21:35 +0300

    Добавление стилей и обработки при не правильности введений данных

--- two-point/src/timer.js
+++ two-point/src/timer.js
@@ -2,21 +2,30 @@ import {
     timer
 } from './shifting.js';
 
-import timerStart, {outPut} from './worktimer.js'
+import timerStart, {
+    outPut
+} from './worktimer.js'
 
 const buttonStart = timer.getElementsByTagName('button')[0];
 const buttonEnd = timer.getElementsByTagName('button')[1];
-console.log(buttonStart, buttonEnd);
+const mes = document.getElementsByClassName('message-error')[0];
+
+// console.log(buttonStart, buttonEnd);
 
 buttonStart.addEventListener('click', () => {
     const inpHour = document.getElementById('hour').value;
     const inpMin = document.getElementById('min').value;
     const inpSec = document.getElementById('sec').value;
 
-    buttonEnd.disabled = false;
-    buttonEnd.innerText = "Пауза";
-    buttonStart.disabled = true;
+    if (inpHour == "" || inpMin == "" || inpSec == "") {
+        mes.style.display = "block";
+    } else {
+        mes.style.display = "none";
+        buttonEnd.disabled = false;
+        buttonEnd.innerText = "Пауза";
+        buttonStart.disabled = true;
 
-    outPut(inpHour, inpMin, inpSec);
-    timerStart(inpHour, inpMin, inpSec);
+        outPut(inpHour, inpMin, inpSec);
+        timerStart(inpHour, inpMin, inpSec);
+    }
 });
