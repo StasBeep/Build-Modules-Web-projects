cadef2247166bd3a6693ec22822d77943035b4d5
StasBeep <stas-stas.stanislav@mail.ru>
2021-06-13 23:05:32 +0300

    Знакомство с gulp произведено В gitignore добавлена сборка (на сервере ей делать нечего) Была загружена библиотека для gulp: gulp-sass "библиотека перерабатывает scss или sass в css и файл отправляется в сборку" Должны в сборку отправиться: стили, картинки, index.html В package.json была добавлена команда для автоматической сборки

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,38 @@
 
 //! В gulp есть множество функций, которые позволяют выполнять какие-то действия
 // В данном формате мы будем из gulp  использовать 2 функции:
-//          src 
-//          dest
-const {src, dest} = require('gulp');
+//          src - определение пути до работы
+//          dest - путь +  папка со сборкой
+const {
+    src,
+    dest
+} = require('gulp');
+
+//* При установке gulp-sass - нужно подключить
+const sass = require('gulp-sass');
 
 // Асинхронные действия (для быстроты действия)
-module.exports.default = function(done) {
+module.exports.default = function (done) {
     console.log('gulp complite')
     //! Чтобы выполнился module, нужно обязательно
     done();
 
     // Для запуска введите в консоли npx gulp
+}
+
+// Превращение стилей scss в css
+module.exports.copy = async function () {
+    return src('scss/index.scss')
+        .pipe(sass())
+        .pipe(dest('build'))
+    // return src('scss/index.scss')
+    //     .pipe(dest('build')) - это не сработает с scss корректно,
+    // если не установить дополнительную библиотеку для gulp
+    //* (в консоли: npm install -D gulp-sass)
+
+    // Для запуска введите в консоли npx gulp copy
+
+
+    //! Нужно build:
+    // Стили, картинки, index.html
 }
