d49165faafa1aaee6f326254166b7a8df8d1f74d
StasBeep <stas-stas.stanislav@mail.ru>
2021-06-13 20:14:37 +0300

    Подготовка для работы с gulp Файлы для работы остались, остальные были удалены Была выполнена команда npm init, для подключения json

--- one-point/AMD/amdindex.js
+++
@@ -1,7 +0,0 @@
-define(["app"], function ({
-    loadScriptArr
-}) {
-    loadScriptArr(['./common.js', './common.js', './timer.js', './a.js'], () => {
-        console.log('loadScriptArr');
-    });
-});
